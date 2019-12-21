1c3f714e29e5a855ffab85003d995e3534856b68
Maciej Loj <maciej.loj@gmail.com>
2019-12-21 15:32:07 +0100

    Update welcome.blade.php
    updated title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    First Site
                 </div>
 
                 <div class="links">
